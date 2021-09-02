f49a4070afae1265db593b77d8c959c537b8202c
suryank <surya14k@gmail.com>
2021-09-02 23:14:23 +0530

    initial

--- vars/customCodeCheckOut.groovy
+++
@@ -1,3 +0,0 @@
-def call() {
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/suryan14k/jenkins-ci-cd-shared-libraries.git']]])
-}
