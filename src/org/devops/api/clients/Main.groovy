cdd17f2340d54fd1125c1ca220b984a9250f7705
suryank <surya14k@gmail.com>
2021-09-03 23:24:22 +0530

    initial

--- src/org/devops/api/clients/Main.groovy
+++ src/org/devops/api/clients/Main.groovy
@@ -1,14 +1,18 @@
 package org.devops.api.clients
 
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
 class Main {
+
     static void main(String[] args) {
-        def username = "***"
-        def password = "***"
+        def username = ""
+        def password = ""
         def organizationId = "c8a97a61-f4c4-4e40-a2b6-ba13718b421c"
         def ownerId = "2cc24e16-4c9c-4ce5-ab0a-346f1d3ed80c"
-        def projectName = "Hello"
+        def projectName = "Test"
         def branch = "master"
-        def apiDirPath = "***"
+        def apiDirPath = "C:\\ci\\api"
 
         def props = [
                 'username': username,
@@ -24,10 +28,11 @@ class Main {
         def apiDesignCenterClient = new ApiDesignCenterClient(this, props)
         def token = apiDesignCenterClient.getAnypointToken()
         def projectId = apiDesignCenterClient.getProjectID(token, projectName)
-        //def branchId = apiDesignCenterClient.getBranchCommitId(token, projectId, branch)
-        //apiDesignCenterClient.branchBackUp(token, projectId, branch, branchId)
-        //apiDesignCenterClient.branchCleanUp(token, projectId, branch)
-        apiDesignCenterClient.addProjectFiles(token,projectId, branch, apiDirPath)
+        def branchId = apiDesignCenterClient.getBranchCommitId(token, projectId, branch)
+        apiDesignCenterClient.branchBackUp(token, projectId, branch, branchId)
+        apiDesignCenterClient.branchCleanUp(token, projectId, branch)
+        apiDesignCenterClient.uploadExchangeDependencyArtifacts(token,projectId, branch, apiDirPath)
+        apiDesignCenterClient.uploadArtifacts(token,projectId, branch, apiDirPath)
         step.println("Design Centre API Upload completed")
     }
 }
